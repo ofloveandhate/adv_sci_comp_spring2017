a6cdd9c194f41a9a369bc70a069a15f162618101
dani brake <danielthebrake@gmail.com>
2017-01-21 21:49:29 -0500

    a little commenting for the function template example

--- templates/basic_function_template.hpp
+++ templates/basic_function_template.hpp
@@ -35,6 +35,9 @@ namespace advscicomp {
 			std::cout << "twiilggut" << '\n';
 	}
 
+
+	// provide an overload of tolower for entire strings. 
+	// beware application of this function to character sets beyond the basics
 #include <algorithm> 
 	std::string tolower(std::string s)
 	{
@@ -42,7 +45,9 @@ namespace advscicomp {
 		return s;
 	}
 
-
+	/**
+	a function which takes a string, and a T2.  does some magic, prints to the screen.
+	*/
 	template <typename T2>
 	void g(std::string const& a, T2 const& b)
 	{
