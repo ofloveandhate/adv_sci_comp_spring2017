b1c6cf889e28e5965d6cc4b8d624e0460159bf4d
dani brake <danielthebrake@gmail.com>
2017-01-21 21:32:26 -0500

    added a few more little toys in the function template example

--- templates/basic_function_template.cpp
+++ templates/basic_function_template.cpp
@@ -7,7 +7,18 @@ int main()
 		advscicomp::max(1,2) << " " << 
 		advscicomp::max(0.1, 100.122) << " " << 
 		advscicomp::max("asdf", "qwer") << '\n';
-		
-	return 0;
+
+std::cout << '\n';
+	advscicomp::g(-1, 100);
+	advscicomp::g(1, 200.11);
+	advscicomp::g(1, -1010l);
+
+std::cout << '\n';
+	advscicomp::g(std::string("adam"), 100);
+	advscicomp::g(std::string("zach"), 200.11);
+	advscicomp::g(std::string("zadie"), -1010l);
+std::cout << '\n';
+
+ 	return 0;
 }
 
--- templates/basic_function_template.hpp
+++ templates/basic_function_template.hpp
@@ -3,7 +3,7 @@
 #pragma once
 
 #include <iostream>
-
+#include <string>
 namespace advscicomp {
 
 
@@ -20,4 +20,41 @@ namespace advscicomp {
 		else
 			return b;
 	}
+
+	/**
+	\brief this function takes two deduced template arguments>
+	*/
+	template <typename T1, typename T2>
+	void g(T1 const& a, T2 const& b)
+	{
+		if (a < 0)
+			std::cout << b << '\n';
+		else if (b/5 > 10)
+			std::cout << a << '\n';
+		else
+			std::cout << "twiilggut" << '\n';
+	}
+
+#include <algorithm> 
+	std::string tolower(std::string s)
+	{
+		std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+		return s;
+	}
+
+
+	template <typename T2>
+	void g(std::string const& a, T2 const& b)
+	{
+
+		std::string m = "meerkat";
+
+		if (tolower(a) < m)
+			std::cout << b << '\n';
+		else if (b/5 > 10)
+			std::cout << a << '\n';
+		else
+			std::cout << "twiilggut" << '\n';
+	}
+
 }
