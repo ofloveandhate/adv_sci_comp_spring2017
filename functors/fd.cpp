aca464b1496fb6549bdf0d3672f37686f8dfc21f
dani brake <danielthebrake@gmail.com>
2017-01-31 16:11:02 -0500

    slight update to functor code

--- functors/fd.cpp
+++ functors/fd.cpp
@@ -25,15 +25,34 @@ void seventh_using_make_fn()
 
 void still_takes_plain_functions()
 {
-	auto psc_o = psc_f(2.0);
 	auto d7_psc_o = make_nth_derivative<7>(sin_plus_cos, 0.00001);
 	std::cout << d7_psc_o(2.) << std::endl;
 }
+
+
+void still_takes_plain_functions2()
+{
+	auto d1_psc_o = make_nth_derivative<1>(cos, 0.00001);
+	std::cout << d1_psc_o(0.) << std::endl;
+}
+
+
+// beware numerical instability!!!
+void still_takes_plain_functions3()
+{
+	auto d1_psc_o = make_nth_derivative<7>(cos, 0.01);
+	std::cout << d1_psc_o(0.) << std::endl;
+}
+
 
 int main()
 {
 	seventh_using_plain_template();
 	seventh_using_make_fn();
 	still_takes_plain_functions();
+	still_takes_plain_functions2();
+	still_takes_plain_functions3();
+
+
 }
 
