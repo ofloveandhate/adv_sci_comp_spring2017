41496d221a00f1b60ca1db0c0580c2228155a274
dani brake on crc <danielthebrake@gmail.com>
2017-03-05 23:13:02 -0500

    added buffering_dependent.cpp

---
+++ mpi/basics/buffering_dependent.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "mpi.h"
+
+/* process 0 receive a number from and send a number from process 1.
+     process 1 receive a number from and send a number to process 0   
+*/ 
+int     main(int argc, char** argv)
+{
+        int my_rank, numbertoreceive, numbertosend = -16;
+        MPI_Status status;
+
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+        if (my_rank==0){
+                MPI_Send( &numbertosend, 1, MPI_INT, 1,    10, MPI_COMM_WORLD);
+                MPI_Recv( &numbertoreceive, 1, MPI_INT, 1,  20, MPI_COMM_WORLD, &status);
+        }
+        else  if(my_rank == 1)
+        {
+                 MPI_Send( &numbertosend,     1, MPI_INT, 0, 20, MPI_COMM_WORLD); 
+                 MPI_Recv( &numbertoreceive, 1, MPI_INT, 0,  10, MPI_COMM_WORLD, &status);
+        }
+        MPI_Finalize();
+        return 0;
+}
