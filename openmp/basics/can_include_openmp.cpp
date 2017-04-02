aeac79f1ee7ee61fd6cdecd8258000351953160a
dani brake <danielthebrake@gmail.com>
2017-04-02 14:25:23 -0400

    added first openmp program

---
+++ openmp/basics/can_include_openmp.cpp
@@ -0,0 +1,14 @@
+// a simple program which does nothing other than prove we can 
+// 1. include OpenMP
+// 2. use a directive from it
+
+#include <omp.h>
+
+#include <cstdlib	>
+
+
+int main() {
+#pragma omp parallel
+    printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
+
+}
