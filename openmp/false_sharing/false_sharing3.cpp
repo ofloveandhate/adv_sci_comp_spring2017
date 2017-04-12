34489434dc6aeae888b246bb3bc33ee408610733
dani brake <danielthebrake@gmail.com>
2017-04-11 21:46:49 -0400

    added false sharing example 3, and updated description in #2

--- openmp/false_sharing/false_sharing3.cpp
+++ openmp/false_sharing/false_sharing3.cpp
@@ -14,9 +14,10 @@ int main(int argc, char ** argv)
 {
   long DIM = 10000;
 
-  // Example 2: improved parallel version (use local accumulators)
+  // Example 3: incorporated random reads
   //
   int odds=0;
+  int result[MAX_NUM_THREADS];
   int *matrix = new int[DIM*DIM]; // represent the square matrix linearly.
   for (long i=0; i<DIM*DIM; ++i)
     matrix[i] = rand();
@@ -26,18 +27,25 @@ int main(int argc, char ** argv)
   // partial count in result[p]
   #pragma omp parallel
   {
-    int odds_mine = 0;
+    int p = omp_get_thread_num();
+    int local = 0;
+    result[p] = 0;
     #pragma omp for
     for( long i = 0; i < DIM; ++i )
     {
       for (long k = 0; k<100; ++k)
       for( long j = 0; j < DIM; ++j )
-        if( matrix[i*DIM + j] % 2 != 0 )
-          ++odds_mine;
+        if ( matrix[i*DIM + j] % 2 != 0 )
+        {
+          if (p%2) // half of accesses are reads
+            ++result[p];
+          else
+            local += result[p];
+        }
     }
 
     #pragma omp atomic
-      odds += odds_mine;
+      odds += result[p];
   }
 
  
