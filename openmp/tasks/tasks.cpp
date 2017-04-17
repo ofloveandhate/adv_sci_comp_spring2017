ef7c0d6d71ad87b315ab65e437d8d766fac3b548
dani brake <danielthebrake@gmail.com>
2017-04-17 17:47:33 -0400

    adjusted task demo so that it generates proof positive that parallelism happened.

--- openmp/tasks/tasks.cpp
+++ openmp/tasks/tasks.cpp
@@ -4,8 +4,16 @@
 // spring 2017
 
 #include <omp.h>
-
+#include <chrono>
 #include <iostream>
+
+template <typename DurationT>
+void WaitFor(DurationT const& t)
+{
+	auto start_time = std::chrono::system_clock::now();
+	while (std::chrono::system_clock::now()-start_time<t)
+	  ; // empty loop statement to do nothing.
+}
 
 
 int foo();
@@ -15,6 +23,7 @@ int bar();
 
 int main()
 {
+	auto start_time = std::chrono::system_clock::now();
 	int x, y;
 #pragma omp parallel
 	#pragma omp single nowait
@@ -29,12 +38,15 @@ int main()
 	}
 
 std::cout << x << std::endl;
+
+std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now()-start_time).count() << " microseconds\n";
 	return 0;
 }
 
 
 int foo()
 {
+	WaitFor(std::chrono::seconds(1));
 	return 1;
 }
 
@@ -42,6 +54,7 @@ int foo()
 
 int bar()
 {
+	WaitFor(std::chrono::seconds(1));
 	return 1;
 }
 
